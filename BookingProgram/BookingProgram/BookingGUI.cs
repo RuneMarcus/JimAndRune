99a01a334e559a7ad7cef9b9e90c1206e829a927
Rune Marcuslund <rune.marcus@gmail.com>
2015-06-15 10:57:34 +0200

    Added login for BookingGUI

--- BookingProgram/BookingProgram/BookingGUI.cs
+++ BookingProgram/BookingProgram/BookingGUI.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.Entity;
 
 namespace BookingProgram
 {
@@ -24,29 +25,62 @@ namespace BookingProgram
 
         private void button1_Click(object sender, EventArgs e)
         {
+            using (Context.BookingContext context = new Context.BookingContext())
+            {
             if (textBox1.Text == "")
             {
-                MessageBox.Show("Indtast en Email!");
+                MessageBox.Show("This is not a valid email.");
             }
             else if (textBox2.Text == "")
             {
-                MessageBox.Show("Indtast et Password!");
+                MessageBox.Show("This password is incorrect!");
             }
             string email1, password1;
 
             email1 = textBox1.Text;
             password1 = textBox2.Text;
 
-            BrugerForm brugerForm = new BrugerForm();
-            AdminForm adminForm = new AdminForm();
+
+            var query = from bruger in context.Brugere
+                        where bruger.email == email1
+                        select bruger;
+
+            BrugerForm brugerForm = new BrugerForm(int loginID);
+            AdminForm adminForm = new AdminForm(int loginID);
 
             this.Hide();
-            adminForm.Show();
-            brugerForm.Show();
+
+
+
+            foreach (var result in query)
+            {
+                if (result.password == textBox2.Text)
+                {
+                    int LoginID = result.brugerID;
+                    if(result.administrator == true)
+                    {
+                        MessageBox.Show("Welcome, (Admin) " + result.fornavn + " " + result.efternavn);
+                        adminForm.Show(result.brugerID);
+                    }
+                    else if (result.administrator == false)
+                    {
+                        MessageBox.Show("Welcome, (User) " + result.fornavn + " " + result.efternavn);
+                        brugerForm.Show(result.brugerID);
+                    }
+                    break;
+                }
+                else
+                {
+                    MessageBox.Show("Unsuccesful login \\n The password is incorrect!");
+                    break;
+                }
+            }
+            
+            
             
 
 
-
+            }
         }
 
         private void button2_Click(object sender, EventArgs e)
