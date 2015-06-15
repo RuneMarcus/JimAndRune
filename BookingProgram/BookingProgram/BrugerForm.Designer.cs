1741a129c6dced925e37a092e16dcc6d5c051ae2
JimP95 <jimpoulsen11@hotmail.com>
2015-06-15 10:57:55 +0200

    SqlCommand Added

--- BookingProgram/BookingProgram/BrugerForm.Designer.cs
+++ BookingProgram/BookingProgram/BrugerForm.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -40,11 +41,22 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Logged ind som Bruger";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Slet min bruger";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // BrugerForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(428, 457);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "BrugerForm";
             this.Text = "BrugerForm";
@@ -56,5 +68,6 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
--- BookingProgram/BookingProgram/BrugerForm.cs
+++ BookingProgram/BookingProgram/BrugerForm.cs
@@ -16,5 +16,20 @@ namespace BookingProgram
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Data.SqlClient.SqlConnection sqlConnection1 =
+                new System.Data.SqlClient.SqlConnection(@"Data Source=np:\\.\pipe\LOCALDB#DF7C19C3\tsql\query;Initial Catalog=Context.BookingContext;Integrated Security=True");
+
+            System.Data.SqlClient.SqlCommand cmd = new System.Data.SqlClient.SqlCommand();
+            cmd.CommandType = System.Data.CommandType.Text;
+            cmd.CommandText = "DELETE Brugere WHERE BrugerID = 5";
+            cmd.Connection = sqlConnection1;
+
+            sqlConnection1.Open();
+            cmd.ExecuteNonQuery();
+            sqlConnection1.Close();
+        }
     }
 }
