833e8a6af92f84fab938510e85d907e4503b1cb6
Rune Marcuslund <rune.marcus@gmail.com>
2015-06-16 12:30:36 +0200

    I said COmmiT!

--- BookingProgram/BookingProgram/BookingForm.Designer.cs
+++ BookingProgram/BookingProgram/BookingForm.Designer.cs
@@ -30,12 +30,14 @@
         {
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.Booking = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(12, 57);
+            this.comboBox1.Location = new System.Drawing.Point(24, 90);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 21);
             this.comboBox1.TabIndex = 0;
@@ -51,16 +53,38 @@
             this.Booking.UseVisualStyleBackColor = true;
             this.Booking.Click += new System.EventHandler(this.Booking_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "LokaleID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(24, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(147, 22);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Lokale Booking";
+            // 
             // BookingForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(426, 310);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.Booking);
             this.Controls.Add(this.comboBox1);
             this.Name = "BookingForm";
             this.Text = "BookingForm";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -68,5 +92,7 @@
 
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Button Booking;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
