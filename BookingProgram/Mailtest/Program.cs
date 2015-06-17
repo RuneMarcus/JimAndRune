7c8124ec3882a9b0c4a15527894ab7e54312ad26
Rune Marcuslund <rune.marcus@gmail.com>
2015-06-17 14:14:13 +0200

    getupbah

--- BookingProgram/Mailtest/Program.cs
+++ BookingProgram/Mailtest/Program.cs
@@ -5,19 +5,46 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net.Mail;
 using System.Net;
+using System.Threading;
+
 
 namespace Mailtest
 {
     class Program
     {
+        Thread th1;
+        Thread th2;
         static void Main(string[] args)
         {
+            // C# code:
 
-            var fromAddress = new MailAddress("from@gmail.com", "From Name");
-            var toAddress = new MailAddress("to@hotmail.com", "To Name");
-            const string fromPassword = "password";
+            using (Imap imap = new Imap())
+            {
+                imap.ConnectSSL("imap.gmail.com");
+                imap.UseBestLogin("pat@gmail.com", "password");
+
+                imap.SelectInbox();
+
+                List<long> uids = imap.Search(Flag.Unseen);
+                foreach (long uid in uids)
+                {
+                    var eml = imap.GetMessageByUID(uid);
+                    IMail mail = new MailBuilder().CreateFromEml(eml);
+
+                    Console.WriteLine(mail.Subject);
+                    Console.WriteLine(mail.Text);
+                }
+                imap.Close();
+            }
+        }
+
+        public void FailResponceMail()
+        {
+            var fromAddress = new MailAddress("voresBookingService@gmail.com", "Darude Booking");
+            var toAddress = new MailAddress("jimpoulsen9@gmail.com", "sandstorm");
+            const string fromPassword = "jimogrune";
             const string subject = "Hello";
-            const string body = "Huehuehue";
+            const string body = "Learn 2 english, plz";
 
             var smtp = new SmtpClient
             {
@@ -28,6 +55,9 @@ namespace Mailtest
                 UseDefaultCredentials = false,
                 Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
             };
+
+
+
             using (var message = new MailMessage(fromAddress, toAddress)
             {
                 Subject = subject,
@@ -36,7 +66,6 @@ namespace Mailtest
             {
                 smtp.Send(message);
             }
-
-        }
+}
     }
 }
